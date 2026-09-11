be49da72c96c4a2da10baff95610175688b2f085
nhi-ngo <nhingo.y@gmail.com>
2026-09-11 16:27:49 -0500

    set up project

---
+++ src/main/java/com/epl/english_premier_league/EnglishPremierLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.epl.english_premier_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishPremierLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishPremierLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epl/english_premier_league/EnglishPremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epl.english_premier_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishPremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
